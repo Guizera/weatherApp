ad64b14e9f1bdba883e2933c061b887058cbfb5c
Jose Guilherme <joseg@octavebio.com>
2025-01-16 15:14:16 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by José Alves da Cunha on 16/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
